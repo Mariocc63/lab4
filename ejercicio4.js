b6c3abe7549fb528035eeec61958f1d28088b2f3
Mariocc63 <mariocldrn@gmail.com>
2018-10-02 18:30:55 -0600

    Update ejercicio4.js

--- ejercicio4.js
+++ ejercicio4.js
@@ -1,6 +1,14 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(JSON.stringify(req.url));
-    res.end();
-}).listen(8080);
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 8000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end(JSON.stringify(req.url));
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
