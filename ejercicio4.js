83c3f30177beb09b298291857ed22fccd3ba648d
Mariocc63 <mariocldrn@gmail.com>
2018-10-02 18:22:30 -0600

    Update ejercicio4.js

--- ejercicio4.js
+++ ejercicio4.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    JSON.stringify(res.write(req.url));
+    res.write(JSON.stringify(req.url));
     res.end();
 }).listen(8080);
